8cf25eb34a00807f9f9d342f1d2c36c3b6fac6bb
Tony Lin <tonylinbusiness@gmail.com>
2020-12-15 08:58:15 -0500

    Added scrolltrigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tonylin';
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit() {
+    this.initialAnimations();
+  }
+
+  initialAnimations(): void {
+    gsap.from(this.document.querySelector('#smallCTA'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('#subhead1'),
+        toggleActions: 'restart pause resume reset',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      x: 200,
+      duration: 0.75,
+      opacity: 0.3,
+    });
+  }
 }
--- src/app/two-col/two-col.component.ts
+++ src/app/two-col/two-col.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-two-col',
   templateUrl: './two-col.component.html',
-  styleUrls: ['./two-col.component.scss']
+  styleUrls: ['./two-col.component.scss'],
 })
 export class TwoColComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
