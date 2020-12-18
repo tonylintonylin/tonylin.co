6c7666033a6347f6bf487be3a855f5975fd635ca
Tony Lin <tonylinbusiness@gmail.com>
2020-12-18 10:43:16 -0500

    added tracker gif

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -38,17 +38,19 @@ export class NavBarComponent implements OnInit {
   iconCSharp: HTMLImageElement;
   iconAngular: HTMLImageElement;
   iconSQL: HTMLImageElement;
+  // svgs: HTMLImageElement;
 
   constructor(@Inject(DOCUMENT) private document) {}
 
   ngOnInit(): void {
     this.html = document.querySelector('html');
     this.img = document.querySelector('img');
-    this.features = document.querySelectorAll('.feature');
     this.switch = document.querySelector('.wrapper');
     this.iconCSharp = document.querySelector('.icon-cSharp');
     this.iconAngular = document.querySelector('.icon-angular');
     this.iconSQL = document.querySelector('.icon-sql');
+    this.features = document.querySelectorAll('.feature');
+    // this.svgs = document.querySelectorAll('.brand-logos');
   }
 
   @HostListener('window:scroll', ['$event'])
@@ -81,6 +83,10 @@ export class NavBarComponent implements OnInit {
       [].forEach.call(this.features, function (feature) {
         feature.style.setProperty('filter', 'invert(1) hue-rotate(180deg)');
       });
+
+      // [].forEach.call(this.svgs, function (svg) {
+      //   svg.style.setProperty('filter', 'invert(1) hue-rotate(180deg)');
+      // });
     } else {
       this.isDay = true;
       this.html.style.setProperty('filter', 'none');
@@ -93,6 +99,10 @@ export class NavBarComponent implements OnInit {
       [].forEach.call(this.features, function (feature) {
         feature.style.setProperty('filter', 'none');
       });
+
+      // [].forEach.call(this.svgs, function (svg) {
+      //   svg.style.setProperty('filter', 'none');
+      // });
     }
   }
 }
